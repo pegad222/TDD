8ee07d7ee87f8dd94593d5d55057fef1a0e97367
daniel.petrovic <daniel@daniel-HP-ProBook-6470b.(none)>
2015-06-29 17:11:42 +0200

    Subir proyecto TDD

---
+++ tdd-app/src/main/java/com/daniel/tdd/App.java
@@ -0,0 +1,13 @@
+package com.daniel.tdd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
